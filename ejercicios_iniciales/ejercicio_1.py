817c27ebb55a76ae36561a551ed4e634adaa0190
nico33k <nicolas.bervel@campus.xaloc.org>
2024-10-21 10:30:21 +0200

    py

--- ejercicios_iniciales/ejercicio_1.py
+++ ejercicios_iniciales/ejercicio_1.py
@@ -1,2 +1,2 @@
-#1.Programa que muestre por pantalla la frase “hello worlddddddd”
+#1.Programa que muestre por pantalla la frase “hello world”
 print ("hello world")
--- ejercicios_iniciales/ejercicio_2.py
+++ ejercicios_iniciales/ejercicio_2.py
@@ -1,4 +1,4 @@
-# Programa que introduzca por tecladoooo tres tipos de variables y se muestren por pantalla en el siguiente orden: número entero, texto y número decimal
+# Programa que introduzca por teclado tres tipos de variables y se muestren por pantalla en el siguiente orden: número entero, texto y número decimal
 var1= int(input("El valor introducido es un: "))
 var2= (input("El valor introducido es la letra: "))
 var3= float(input("El valor introducido es un: "))
