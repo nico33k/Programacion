38e14ce928cb8ac34842238a152f84fb50499aa8
nico33k <nicolas.bervel@campus.xaloc.org>
2025-01-25 16:46:04 +0100

    Update ejercicio_66.py

--- ejercicios_while/ejercicio_66.py
+++ ejercicios_while/ejercicio_66.py
@@ -1,3 +1,4 @@
+# 66. Repite el ejercicio 63. En lugar de ‘tirar’ 100 veces un dado, modifica el programa para ver cómo se comporta el dado en lanzamientos producidos durante aprox 3 segundos. 
 import random
 import time
 t_inicial = time.time()
