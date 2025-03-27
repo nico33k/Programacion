b9315066c61f0a44cfc81d0cf884d0a1362bf06f
nico33k <nicolas.bervel@campus.xaloc.org>
2025-03-27 09:21:28 +0100

    python

--- ejemplos/ejemplo_while.py
+++
@@ -1,7 +0,0 @@
-contador=1
-pares = 0
-while contador<=20:
-    if contador%2 == 0:
-        pares +=1
-    contador+=1
-print('El número de números pares es: ', pares)
--- ejemplos/ejemplo_while_2.py
+++
@@ -1,9 +0,0 @@
-import random
-num_secret = random.randint(1, 100)
-num_usuario = 0
-while num_usuario != num_secret:
-    num_usuario = int(input('Adivine el número secreto: '))
-    if num_usuario != num_secret:
-        print('No lo has adivinado')
-    if num_usuario == num_secret:
-        print('Lo has adivinado, felicidades')
