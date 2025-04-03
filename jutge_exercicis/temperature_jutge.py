0a91e5499e07615ab121c68c5c7bb1f14a366977
nico33k <nicolas.bervel@campus.xaloc.org>
2025-04-03 22:04:41 +0200

    Add files via upload

---
+++ jutge_exercicis/temperature_jutge.py
@@ -0,0 +1,11 @@
+degree = int(input())
+if degree>30:
+    print ("it's hot")
+if degree <= 30 and degree >= 10:
+    print("it's ok")
+if degree < 10:
+    print("it's cold")
+if degree >= 100:
+    print('water would boil')
+if degree <= 0:
+    print('water would freeze')
